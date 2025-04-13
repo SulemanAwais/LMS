c3a6f5ed1975ba86c89e948e0dcca06859c16629
SulemanAwais <sulemanawais2001@gmail.com>
2025-04-13 20:21:09 +0500

    Update settings.py

--- library_mgmt/library_mgmt/settings.py
+++ library_mgmt/library_mgmt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mlz@@fllba^sruw&=b9z*1h2^8wn&r!cupa=9@_m3kf(87=14t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
